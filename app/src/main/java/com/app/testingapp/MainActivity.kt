17c474857b893d8014ce0b0a6c71ef34bc845055
Aditya Cheke <81345503+chekeAditya@users.noreply.github.com>
2023-01-30 23:04:19 +0530

    material design

--- app/src/main/java/com/app/testingapp/MainActivity.kt
+++ app/src/main/java/com/app/testingapp/MainActivity.kt
@@ -1,12 +1,16 @@
 package com.app.testingapp
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -15,12 +19,17 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.app.testingapp.ui.theme.TestingAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MessageCard(Message(author = "Aditya", "I've wrote many articles and books"))
+            TestingAppTheme {
+                Surface {
+                    MessageCard(Message(author = "Aditya", "I've wrote many articles and books"))
+                }
+            }
         }
     }
 }
@@ -36,21 +45,46 @@ fun MessageCard(msg: Message) {
                 .size(40.dp)
                 .clip(CircleShape)
                 .background(Color.Black)
+                .border(1.5.dp, MaterialTheme.colors.secondary, CircleShape)
         )
         // Add a horizontal space between the image and the column
         Spacer(modifier = Modifier.width(10.dp))
         Column {
-            msg.author?.let { Text(text = it) }
+            msg.author?.let {
+                Text(
+                    text = it,
+                    color = MaterialTheme.colors.secondaryVariant,
+                    style = MaterialTheme.typography.subtitle2
+                )
+            }
             Spacer(modifier = Modifier.height(5.dp))
-            msg.body?.let { Text(text = it) }
+            Surface(shape = MaterialTheme.shapes.medium, elevation = 5.dp) {
+                msg.body?.let {
+                    Text(
+                        text = it,
+                        color = MaterialTheme.colors.onSecondary,
+                        style = MaterialTheme.typography.body2,
+                        modifier = Modifier.padding(all = 5.dp)
+                    )
+                }
+            }
         }
     }
 }
 
-@Preview(showBackground = true)
+@Preview(name = "Light Mode")
+@Preview(
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+    showBackground = true,
+    name = "Dark Mode"
+)
 @Composable
 fun PreviewMessageCard() {
-    MessageCard(Message(author = "Aditya", "I've wrote many articles and books"))
+    TestingAppTheme {
+        Surface {
+            MessageCard(Message(author = "Aditya", "I've wrote many articles and books"))
+        }
+    }
 }
 
 data class Message(
