347dc902b69d5f50e208485bdbc49d0c0a887360
Aditya Cheke <81345503+chekeAditya@users.noreply.github.com>
2023-01-30 23:45:44 +0530

    list and animation

--- app/src/main/java/com/app/testingapp/MainActivity.kt
+++ app/src/main/java/com/app/testingapp/MainActivity.kt
@@ -4,15 +4,19 @@ import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateColorAsState
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
@@ -27,7 +31,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             TestingAppTheme {
                 Surface {
-                    MessageCard(Message(author = "Aditya", "I've wrote many articles and books"))
+                    Conversation(SampleData.conversions())
                 }
             }
         }
@@ -49,7 +53,13 @@ fun MessageCard(msg: Message) {
         )
         // Add a horizontal space between the image and the column
         Spacer(modifier = Modifier.width(10.dp))
-        Column {
+
+        // We keep track if the message is expanded or not in this variable
+        var isExpanded by remember { mutableStateOf(false) }
+        val surfaceColor by animateColorAsState(
+            if (isExpanded) MaterialTheme.colors.primary else MaterialTheme.colors.secondary,
+        )
+        Column(modifier = Modifier.clickable { isExpanded = !isExpanded }) {
             msg.author?.let {
                 Text(
                     text = it,
@@ -62,12 +72,25 @@ fun MessageCard(msg: Message) {
                 msg.body?.let {
                     Text(
                         text = it,
-                        color = MaterialTheme.colors.onSecondary,
+//                        color = MaterialTheme.colors.onSecondary,
+                        //here i've to update the color
+                        color = surfaceColor,
                         style = MaterialTheme.typography.body2,
-                        modifier = Modifier.padding(all = 5.dp)
+                        modifier = Modifier.padding(all = 5.dp),
+                        // If the message is expanded, we display all its content otherwise we only display the first line
+                        maxLines = if (isExpanded) Int.MAX_VALUE else 1,
                     )
                 }
             }
+        }
+    }
+}
+
+@Composable
+fun Conversation(message: List<Message>) {
+    LazyColumn {
+        items(message) { message ->
+            MessageCard(message)
         }
     }
 }
@@ -82,7 +105,7 @@ fun MessageCard(msg: Message) {
 fun PreviewMessageCard() {
     TestingAppTheme {
         Surface {
-            MessageCard(Message(author = "Aditya", "I've wrote many articles and books"))
+            Conversation(SampleData.conversions())
         }
     }
 }
@@ -92,3 +115,28 @@ data class Message(
     val body: String? = null,
     val description: String? = null
 )
+
+
+class SampleData {
+    companion object {
+        @Composable
+        fun conversions(): List<Message> {
+            return listOf(
+                Message(
+                    author = "Aditya",
+                    "I've wrote many articles and books asdchkasdcj adscjnjajlsd adscnajsncdla  asdnclnalsdc asdlcna dcajlndcljasj casljc ajs dc ljadsjc ajdscl ajls cdjaljsdc ja scjljas clj asd"
+                ),
+                Message(author = "Karan", "I've wrote many artic aldscnals caljsdjc lajs cdj ajlsdc  ajlds ncj asnd cja sd la sdcl jasd cjl asdl ljasdl asdljv la sdv asdles"),
+                Message(author = "Prachi", "How are you, how's life going ? ajsdcnjalsd casdc asdc asdc a dc asdc adsc adsc asdc asdc asdc adsc asdc asdc "),
+                Message(
+                    author = "Anjali",
+                    "I am getting so bored will you wantcasdlkc asm,dc asd cads cads csad ca sdca sdc a dva fdb  gabdfsdfdagsgn bdags bsff dbsds gbgsd bfvffdv fdbvsfs abvsf sb vf sbvsf sbfs sfb dd bdfsv bv  be to entertain you"
+                ),
+                Message(
+                    author = "Varun",
+                    "Can you pcda dc ac fsvds cdsavfbvss sbgd bs fd sbgd sds bg dd bsfd f dbd sf sbsfd f dbf  bffs b dsfsbfdf d fdf d fdvdb vf fdv f vdb f vdfsbfvds lease tell me about yourself"
+                ),
+            )
+        }
+    }
+}
